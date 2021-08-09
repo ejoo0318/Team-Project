2b3ed6c30781ed85d167d2f992429e28903b16d7
ejoo0318 <ejoo0318@naver.com>
2021-08-09 15:15:57 +0900

    업데이트

---
+++ static/js/btn.js
@@ -0,0 +1,7 @@
+$(function (event) {
+    $('#back-btn').on('click', function (event) {
+        if(window.location == "http://localhost:8000/community/tips/tips_detail/") {
+
+        }
+    })
+})
